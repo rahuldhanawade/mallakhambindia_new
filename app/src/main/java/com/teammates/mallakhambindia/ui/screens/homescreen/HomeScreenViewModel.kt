b08b96568a8e1fa59c97dbd6dd2d74604f166f92
rahuldhanawade <60505899+rahuldhanawade@users.noreply.github.com>
2025-04-07 14:59:25 +0530

    homescreen added

--- app/src/main/java/com/teammates/mallakhambindia/ui/screens/homescreen/HomeScreenViewModel.kt
+++ app/src/main/java/com/teammates/mallakhambindia/ui/screens/homescreen/HomeScreenViewModel.kt
@@ -1,4 +1,30 @@
 package com.teammates.mallakhambindia.ui.screens.homescreen
 
-class HomeScreenViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.teammates.mallakhambindia.data.ResponseModel.UserDetailsResponse
+import com.teammates.mallakhambindia.repository.MainRepository
+import com.teammates.mallakhambindia.utils.SharedPreferencesHelper
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeScreenViewModel @Inject constructor(
+    private val sharedPreferencesHelper: SharedPreferencesHelper,
+    private val mainRepository: MainRepository
+) : ViewModel() {
+
+    val TAG = "HomeScreenViewModel"
+
+    val userDetailsResponse =  sharedPreferencesHelper.getString("userDetails")
+
+    val gson = Gson()
+
+    val judgeData = gson.fromJson(userDetailsResponse, UserDetailsResponse::class.java)
+
+    fun logout(){
+        sharedPreferencesHelper.clear()
+    }
+
 }
--- app/src/main/java/com/teammates/mallakhambindia/ui/screens/homescreen/homescreen.kt
+++ app/src/main/java/com/teammates/mallakhambindia/ui/screens/homescreen/homescreen.kt
@@ -8,9 +8,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.outlined.ExitToApp
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -19,8 +31,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -29,31 +46,53 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.teammates.myapplication.ui.theme.primaryTextColor
+import com.teammates.myapplication.ui.theme.white
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(navController: NavHostController) {
+fun HomeScreen(navController: NavHostController, viewModel: HomeScreenViewModel = hiltViewModel()) {
 
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+    val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
+    var showBottomSheet by remember { mutableStateOf(false) }
 
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
-                CompetitionBanner()
+                CompetitionBanner(viewModel)
                 NavigationDrawerItem(
-                    label = { Text("Item 1") },
+                    label = { Text("About Us") },
+                    icon =  {
+                        Icon(
+                            imageVector = Icons.Outlined.Info,
+                            contentDescription = "about us",
+                            modifier = Modifier.padding(5.dp)
+                        )
+                    },
                     selected = false,
                     onClick = { /* Handle click */ }
                 )
                 NavigationDrawerItem(
-                    label = { Text("Item 2") },
+                    label = { Text("Log Out") },
+                    icon =  {
+                        Icon(
+                            imageVector = Icons.Outlined.ExitToApp,
+                            contentDescription = "about us",
+                            modifier = Modifier.padding(5.dp)
+                        )
+                    },
                     selected = false,
-                    onClick = { /* Handle click */ }
+                    onClick = {
+                            viewModel.logout()
+                    }
                 )
             }
         },
@@ -81,24 +120,60 @@ fun HomeScreen(navController: NavHostController) {
                         }
                     },
                     colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = primaryTextColor, // Set the background color
+                        containerColor = Color.Black, // Set the background color
                         titleContentColor = Color.White, // Set the title color
                         actionIconContentColor = Color.White // Set action icon color
                     )
                 )
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { showBottomSheet = true },
+                    containerColor = primaryTextColor,
+                    contentColor = white,
+                    shape = CircleShape
+                ) {
+                    Icon(
+                        Icons.Outlined.Menu,
+                        "Floating action button."
+                    )
+                }
             }
         ){padding ->
             Box(
                 modifier = Modifier.padding(padding)
+                    .background(Color.Black),
+                contentAlignment = Alignment.TopStart,
             ){
-
+                Column{
+                    HomeRadioGroup()
+                    ListOfTeams()
+                }
+            }
+            if (showBottomSheet) {
+                ModalBottomSheet(
+                    onDismissRequest = {
+                        showBottomSheet = false
+                    },
+                    sheetState = sheetState
+                ) {
+                    Button(onClick = {
+                        scope.launch { sheetState.hide() }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                showBottomSheet = false
+                            }
+                        }
+                    }) {
+                        Text("Hide bottom sheet")
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun CompetitionBanner() {
+fun CompetitionBanner(viewModel: HomeScreenViewModel) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -131,11 +206,159 @@ fun CompetitionBanner() {
             )
         }
         Text(
-            text = "",
+            text = viewModel.judgeData.judge?.name.toString(),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             color = Color.White,
             modifier = Modifier.padding(top = 20.dp)
+        )
+    }
+}
+
+@Composable
+fun CustomRadioGroup() {
+
+    val options = listOf(
+        "In Complete",
+        "Completed",
+    )
+
+    var selectedOption by remember { mutableStateOf("") }
+    val onSelectionChange = { text: String ->
+        selectedOption = text
+    }
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        options.forEach { text ->
+            Column(
+                modifier = Modifier
+                    .padding(
+                        all = 8.dp,
+                    ),
+            ) {
+                Text(
+                    text = text,
+                    style = typography.bodySmall,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .clip(
+                            shape = RoundedCornerShape(
+                                size = 12.dp,
+                            ),
+                        )
+                        .clickable {
+                            onSelectionChange(text)
+                        }
+                        .background(
+                            if (text == selectedOption) {
+                                primaryTextColor
+                            } else {
+                                white
+                            }
+                        )
+                        .padding(
+                            vertical = 12.dp,
+                            horizontal = 16.dp,
+                        ),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeRadioGroup() {
+
+    val incomp = "In Complete"
+    val comp = "Completed"
+
+    var selectedOption by remember { mutableStateOf("") }
+    val onSelectionChange = { text: String ->
+        selectedOption = text
+    }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Box(
+            modifier = Modifier.weight(1f)
+                .padding(5.dp)
+        ){
+            Text(
+                text = incomp,
+                style = typography.bodySmall,
+                color = Color.Black,
+                modifier = Modifier
+                    .clip(
+                        shape = RoundedCornerShape(
+                            size = 12.dp,
+                        ),
+                    )
+                    .clickable {
+                        onSelectionChange(incomp)
+                    }
+                    .background(
+                        if (incomp == selectedOption) {
+                            primaryTextColor
+                        } else {
+                            white
+                        }
+                    )
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+            )
+        }
+        Box(
+            modifier = Modifier.weight(1f)
+                .padding(5.dp)
+        ){
+            Text(
+                text = comp,
+                style = typography.bodySmall,
+                color = Color.Black,
+                modifier = Modifier
+                    .clip(
+                        shape = RoundedCornerShape(
+                            size = 12.dp,
+                        ),
+                    )
+                    .clickable {
+                        onSelectionChange(comp)
+                    }
+                    .background(
+                        if (comp == selectedOption) {
+                            primaryTextColor
+                        } else {
+                            white
+                        }
+                    )
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+@Composable
+fun ListOfTeams() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(Color.White)
+            .padding(top = 5.dp)
+            .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)),
+    ){
+        Text(
+            text = "HomeScreen",
+            style = typography.bodySmall,
+            color = Color.Black,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
         )
     }
 }
